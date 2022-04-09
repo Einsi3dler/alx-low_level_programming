7faf8cfe4a2cf2313f7b39acb919a5d9f720d5ed
Sylvester_Divine <vertigosylvester@gmail.com>
2022-04-09 12:20:37 -0700

    My ninth commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/* more headers goes there*/
+/*betty style doc for function main goes there*/
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ch;
+	int CH;
+
+	for (ch = '1'; ch <= '9'; ch++)
+	putchar(ch);
+	for (CH = 'a'; CH <= 'f'; CH++)
+	putchar(CH);
+	putchar('\n');
+	return (0);
+}
