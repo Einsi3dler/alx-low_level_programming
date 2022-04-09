956f53f02d9b176bb5bbfd60e4a5e655efb1e95b
Sylvester_Divine <vertigosylvester@gmail.com>
2022-04-09 10:45:20 -0700

    Fourth commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/* more headers goes there*/
+/*betty style doc for function main goes there*/
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ch;
+	int CH;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	putchar(ch);
+	for (CH = 'A'; CH <= 'Z'; CH++)
+	putchar(CH);	
+	putchar('\n');
+	return (0);
+}
