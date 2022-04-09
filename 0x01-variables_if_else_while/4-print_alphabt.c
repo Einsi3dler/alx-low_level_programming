8ebbfa44a44e99c3397ba190186241f5e53b0e21
Sylvester_Divine <vertigosylvester@gmail.com>
2022-04-09 11:24:01 -0700

    My Fifth commit

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,15 +10,15 @@ int main(void)
 {
 	int ch = 'a';
 
-	while(ch <= z )
+	while(ch <= 'z' )
 	{
 		if (ch == 'e' || ch == 'q')
 		{
 			ch++;
 		}
 		else
-			putchar(ch);
-			ch++;
+		putchar(ch);
+		ch++;
 	}
 	putchar('\n');
 	return (0);
