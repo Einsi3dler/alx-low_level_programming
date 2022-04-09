252d11c00bafc3a74c02c00cc7854129ab05d64f
Sylvester_Divine <vertigosylvester@gmail.com>
2022-04-09 11:21:20 -0700

    My Fifth commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/* more headers goes there*/
+/*betty style doc for function main goes there*/
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ch = 'a';
+
+	while(ch <= z )
+	{
+		if (ch == 'e' || ch == 'q')
+		{
+			ch++;
+		}
+		else
+			putchar(ch);
+			ch++;
+	}
+	putchar('\n');
+	return (0);
+}
