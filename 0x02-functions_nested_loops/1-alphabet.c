9c4de067ebd4e12424719cd14cf3a6fd9d7d9218
Sylvester_Divine <vertigosylvester@gmail.com>
2022-04-13 02:31:21 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+void print_alphabet(void)
+{
+	int ch;
+
+	for(ch = 'a'; ch >= 'z'; ch++)
+	{
+		_puthcar(ch);
+	}
+	_putchar(ch);
+}
+	
