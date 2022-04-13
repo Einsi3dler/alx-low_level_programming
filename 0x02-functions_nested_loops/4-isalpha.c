9382135f3146f475445b3d4cee236ec8838938ec
Sylvester_Divine <vertigosylvester@gmail.com>
2022-04-13 09:57:18 -0700

    Fifth

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * _islower - selecting lower cases
+ * _isalpha - selecting alphabets
  *@c: contain value to be compared
  * Return: Always 0.
  */
 
 int _isalpha(int c)
 {
-	if (((c >= 'a') & (c <= 'z'))|((c >= 'A') & (c <= 'Z')))
+	if (((c >= 'a') & (c <= 'z')) | ((c >= 'A') & (c <= 'Z')))
 	{
 		return (1);
 	}
