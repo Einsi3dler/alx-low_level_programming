fdf46b101aef581cdb2e7fbd338ced10caeb3f50
Sylvester_Divine <vertigosylvester@gmail.com>
2022-04-13 09:30:28 -0700

    Fourth

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet
+ *
+ * Return: Always 0.
+ */
+
+int _islower(int c)
+{
+	int one = 1;
+	int zero = 0;
+
+	if (c >= 97 & c <= 122 )
+	{
+		return one;
+	}
+	else
+	{
+		return zero;
+	}
+}
