1c63e034000d46f54bcc4f61f71a4d92eff14c7b
Sylvester_Divine <vertigosylvester@gmail.com>
2022-04-13 09:39:03 -0700

    Fourth

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - print alphabet
- *
+ * _islower - selecting lower cases
+ *@c: contain value to be compared
  * Return: Always 0.
  */
 
@@ -11,12 +11,12 @@ int _islower(int c)
 	int one = 1;
 	int zero = 0;
 
-	if ((c >= 97) & (c <= 122) )
+	if ((c >= 97) & (c <= 122))
 	{
-		return one;
+		return (one);
 	}
 	else
 	{
-		return zero;
+		return (zero);
 	}
 }
