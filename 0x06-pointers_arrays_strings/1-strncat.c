ff576ef59a450ca2377dde569033c6255d8b9d5e
Sylvester_Divine <vertigosylvester@gmail.com>
2022-04-23 09:18:04 -0700

    An experiment

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ *_strcat - concatinate 2 strings.
+ *@dest: first string.
+ *@src: second string.
+ *Return: string.
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0, j = 0;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+	}
+
+	for (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+		if (j == n)
+		{
+			break;
+		}
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+}
