41ce5e1864cb1b7f90be88afbfa6344344975e56
sylvester_divine <sylvesterdivine@gmail.com>
2022-05-01 20:00:11 +0100

    added c file to determine factorial recursive

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/*
+factorial  - Gets the factorial of any number
+@n: integer n 
+Return : Yes
+*/
+
+int factorial(int n);
+{
+	if (n == 1)
+	{
+		return (1);
+	}
+	if  (n == 0 )
+	{
+		return (1);
+	}
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (n * factorial(n-1));
+	}
+}
