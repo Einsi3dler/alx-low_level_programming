5fbb30092bbe9956b71b45383d9668ccacf0be75
Sylvester_Divine <vertigosylvester@gmail.com>
2022-05-03 18:30:28 -0700

    notepad redo

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ *main - sums the number in the arguments ,  followed by a new line.
+ *@argc: number of arguments in the command line
+ *@argv: array that contains the arguments in the command line
+ *Return: Return returns 0
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+	int j, sum = 0;
+
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (j = 1; j < argc; j++)
+	{
+		sum = sum + atoi(argv[j]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
