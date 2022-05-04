9acb58310cf62e6b02802b96e47bf81a1ad94f70
Sylvester_Divine <vertigosylvester@gmail.com>
2022-05-03 17:18:42 -0700

    aced argc multiplication comit

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - sums the number in the arguments ,  followed by a new line.
+ *@argc: number of arguments in the command line
+ *@argv: array that contains the arguments in the command line
+ *Return: Return returns 0
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, sum = 1;
+
+	if (argc < 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		sum = sum * atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
