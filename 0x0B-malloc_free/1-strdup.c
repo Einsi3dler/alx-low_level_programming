156318d97c2878b58b74cef81ec51138791a31cc
sylvester_divine <sylvesterdivine@gmail.com>
2022-05-05 22:36:49 +0100

    first test of 1 malloc

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup - duplicates the string and return the pointer address.
+ * @str: string to be copied.
+ *Return: pointer of an array of chars
+ */
+ char *_strdup(char *str)
+ {
+	 char *cr;
+	 unsigned int i, p;
+	 
+	 for (i = 0; str[i] == '\0'; i++)
+	 {		 
+	 }
+	 i = i + 1; 
+	 cr = (char *)malloc(i * sizeof(char));
+	 for (p = 0; p <= i; p++)
+	 {
+		 str[p] = cr[p];
+	 }
+	 return (cr);
+ }
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,2 @@
+This directory contains the excersies done by me and given by holberton on the assignment dynamic memory allocation
+specifically malloc
