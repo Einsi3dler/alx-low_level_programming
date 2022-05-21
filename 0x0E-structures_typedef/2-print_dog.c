f086f3a8d5a40435545c350a4a4b4a4e57afc352
Sylvester_Divine <vertigosylvester@gmail.com>
2022-05-21 10:18:30 -0700

    second batch undone, switched to local ide

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,7 +13,7 @@ void print_dog(struct dog *d)
 	}
 	if (d->name == NULL)
 	{
-		printf("Name: (nil)");
+		printf("Name: (nil)\n");
 	}
 	else
 	{
@@ -22,7 +22,7 @@ void print_dog(struct dog *d)
 
 	if (d->age == -0.0)
 	{
-		printf("Age: (nil)");
+		printf("Age: (nil)\n");
 	}
 	else
 	{
@@ -30,7 +30,7 @@ void print_dog(struct dog *d)
 	}
 	if (d->owner == NULL)
 	{
-		printf("Owner: (nil)");
+		printf("Owner: (nil)\n");
 	}
 	else
 	{
