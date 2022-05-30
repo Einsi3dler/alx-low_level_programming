ab1b652843f3b04d27a4d3202fd1017014b4fd21
Sylvester_Divine <vertigosylvester@gmail.com>
2022-05-30 02:30:16 -0700

    rodeo testing

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+*print_listint - to print all the elements in a list using putchar
+*@h: the head of the list
+*Return: Returns number of elements in list
+*/
+
+size_t print_listint(const listint_t *h)
+{
+	size_t count_e;
+	
+	count_e = 0;
+	while (h != NULL)
+	{
+		if (h->n == NULL)
+		{
+			Return (NULL)
+		}
+        else
+        {
+            printf("%d\n" , h->n);
+        }
+        h = h->next;
+        count_e++;
+    }
+    return (count_e);
+}
