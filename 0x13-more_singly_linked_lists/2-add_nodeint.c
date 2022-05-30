d09cc8cb1668870f81b73cf2dd0ebbdef1cd1904
sylvester_divine <sylvesterdivine@gmail.com>
2022-05-30 12:17:32 +0100

    13 , 4 for 3

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -2,11 +2,13 @@
 /**
  * add_nodeint - adds node at the head
  * @head: the address of the head
+ * @n: contains the data to be put in the node
  * Return: returns the address of the head
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *temp, *ptr;
+
 	ptr = malloc(sizeof(listint_t));
 
 	if (ptr == NULL)
