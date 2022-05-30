80a5a34dc53149db8cb7f293278b8a3b5d91794e
sylvester_divine <sylvesterdivine@gmail.com>
2022-05-30 11:24:54 +0100

    7 push ,1 for 2

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ *listint_len - Returns the number of lists
+ *@h: head of single list
+ *Return: Return total list of elements
+ *
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t count_e;
+
+	count_e = 0;
+
+	while (h != NULL)
+	{
+			h = h->next;
+			count_e++;
+	}
+	return (count_e);
+}
