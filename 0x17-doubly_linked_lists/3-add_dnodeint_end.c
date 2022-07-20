527aa154ec351f9b15d48ca0ae1e99781d85e151
Sylvester_Divine <sylvesterdivine@outlook.com>
2022-07-20 23:27:46 +0000

    add node to end, task push

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -25,7 +25,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	if (*head == NULL)
 	{
 		*head  = end_node;
-		return (guide);
+		return (end_node);
 	}
 	else
 	{
@@ -35,6 +35,6 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 		}
 		guide->next = end_node;
 		end_node->prev = guide;
-		return (guide->next);
+		return (end_node);
 	}
 }
