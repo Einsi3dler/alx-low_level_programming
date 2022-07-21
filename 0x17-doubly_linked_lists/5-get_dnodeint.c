ef3691a698290942126274fed176409816e39a50
Sylvester_Divine <sylvesterdivine@outlook.com>
2022-07-21 21:31:10 +0000

    get node, task push

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -16,12 +16,12 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	i = 0;
 	while (i != index)
 	{
+		if (holder == NULL)
+		{
+			return (NULL)
+		}
 		holder = holder->next;
 		i++;
-		if (holder->next == NULL)
-		{
-			return (NULL);
-		}
 	}
 	return (holder);
 }
