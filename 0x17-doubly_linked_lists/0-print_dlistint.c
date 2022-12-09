14de5cf5d0ef008834ccdbea02c1df352539a012
sylvester_divine <sylvesterdivine@outlook.com>
2022-12-09 20:17:57 +0000

    1 refactor

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -4,17 +4,18 @@
  * @h: the pointer to the head node
  * Return: This returns the the number of nodes
  */
- size_t print_dlistint(const dlistint_t *h) {
-	 const dlistint_t *temp;
-	 size_t node_Num;
+size_t print_dlistint(const dlistint_t *h)
+{
+	const dlistint_t *temp;
+	size_t node_Num;
 
-	 node_Num = 0;
-	 temp = h;
-	 while( temp != NULL)
-	 {
-		 printf("%d\n",temp->n);
-		 temp = temp->next;
-		 node_Num ++;
-	 }
-	 return (node_Num);
- }
+	node_Num = 0;
+	temp = h;
+	while (temp != NULL)
+	{
+		printf("%d\n", temp->n);
+		temp = temp->next;
+		node_Num++;
+	}
+	return (node_Num);
+}
