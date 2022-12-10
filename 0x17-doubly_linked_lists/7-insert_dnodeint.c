a32d39127d2bddeba049b6553e91838a85502784
sylvester_divine <sylvesterdivine@outlook.com>
2022-12-10 18:02:52 +0000

    7 file refactor

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -9,7 +9,7 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *ptr, *temp, *new_node;
+	dlistint_t *ptr, *new_node;
 	unsigned int index;
 
 	index = 0;
