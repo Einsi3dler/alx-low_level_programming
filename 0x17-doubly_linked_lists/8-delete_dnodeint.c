2066e82d215b4a2b4252291d58d03d620941a411
sylvester_divine <sylvesterdivine@outlook.com>
2022-12-10 18:10:20 +0000

    file fix

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -19,13 +19,15 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		temp = temp->next;
 		count++;
 	}
+
 	if (temp == ptr)
 		return (1);
 	if (count == 1)
+	{
 		free(*head);
 		*head = NULL;
 		return (1);
-
+	}
 	if (index == 0)
 	{
 		*head = ptr->next;
