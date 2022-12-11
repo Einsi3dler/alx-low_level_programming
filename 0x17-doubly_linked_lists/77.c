9e85230bd6ad917947af8f69fc1a05c496649e2b
sylvester_divine <sylvesterdivine@outlook.com>
2022-12-11 22:31:22 +0000

    test file creation

---
+++ 0x17-doubly_linked_lists/77.c
@@ -0,0 +1,49 @@
+#include "lists.h"
+/**
+ * insert_dnodeint_at_index - this inserts a node at a specific index
+ * @h: contains the address of the head node
+ * @idx: index point
+ * @n: int to be inserted into the new node
+ * Return: this returns the new address of the node
+ */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *ptr, *new_node;
+	unsigned int index;
+
+	index = 0;
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	ptr = *h;
+	while (index != idx)
+	{
+		ptr = ptr->next;
+		index++;
+	}
+	new_node->n = n;
+	new_node->next = ptr;
+	if (ptr == NULL)
+	{
+		new_node->prev = NULL;
+		*h = new_node;
+		return (new_node);
+	}
+	else if (idx == 0)
+	{
+		new_node->prev = NULL;
+		ptr->prev = new_node;
+		*h = new_node;
+		return (new_node);
+	}
+	else
+	{
+		new_node->prev = ptr->prev;
+		ptr->prev->next = new_node;
+		ptr->prev = new_node;
+		return (new_node);
+	}
+}
