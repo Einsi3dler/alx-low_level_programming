17a512f7d2f06984f99443fe5275a86715770f24
Sylvester_Divine <sylvesterdivine@outlook.com>
2023-07-13 08:29:17 +0000

    betty checks and reefixes to code base

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -5,20 +5,19 @@
  * @l: low p[oint
  * @h: high point
  */
-
 void print_array(int *a, int l, int h)
 {
-        int i;
+	int i;
 
-        for (i = l; i < h; i++)
-        {
-                printf("%d", a[i]);
-                if (i < h - 1)
-                {
-                        printf(", ");
-                }
-        }
-        printf("\n");
+	for (i = l; i < h; i++)
+	{
+		printf("%d", a[i]);
+		if (i < h - 1)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
 }
 
 /**
@@ -30,38 +29,37 @@ void print_array(int *a, int l, int h)
  */
 int binary_search(int *array, size_t size, int value)
 {
-        size_t low, high, mid, index;
+	size_t low, high, mid, index;
 
-        if (array == NULL)
-        {
-                return (-1);
-        }
-        low = 0;
-        high = size;
+	if (array == NULL)
+	{
+		return (-1);
+	}
+	low = 0;
+	high = size;
 
-        while (low != high)
-        {
-                mid = ((high + 1) - low) / 2;
-                mid = mid - 1;
-                index = low + mid;
-                printf("Searching in array: ");
-                print_array(array, low, high);
-                if (array[index] == value)
-                {
-                        return (index);
-                }
-                else
-                {
-                        if (array[index] > value)
-                        {
-                                high = index;
-                        }
-                        else if (array[index] < value)
-                        {
-                                low = index + 1;
-                        }
-                }
-        }
-        return (-1);
+	while (low != high)
+	{
+		mid = ((high + 1) - low) / 2;
+		mid = mid - 1;
+		index = low + mid;
+		printf("Searching in array: ");
+		print_array(array, low, high);
+		if (array[index] == value)
+		{
+			return (index);
+		}
+		else
+		{
+			if (array[index] > value)
+			{
+				high = index;
+			}
+			else if (array[index] < value)
+			{
+				low = index + 1;
+			}
+		}
+	}
+	return (-1);
 }
-
